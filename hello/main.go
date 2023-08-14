18c8227e7d084ca710dcd5973a25668e6b69e7e9
poorvaditya behre <aditya.behre@gmail.com>
2023-08-15 01:17:18 +0530

    updated Readme, added overview about dataTypes

--- hello/main.go
+++ hello/main.go
@@ -1,10 +1,14 @@
 // package name - main
+// you can wrap the functionalities in a package
 package main
 
 // import "fmt" library 
 import "fmt"
 
 
+// declaring functions ->
+// This is the main function just like you have in c/c++ . 
+// main() function acts as an starting point for your application
 func main()  {
 	fmt.Println("hello World from GO !!");
 }
